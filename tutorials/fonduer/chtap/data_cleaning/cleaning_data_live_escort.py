dabe005c7c0f4364ac417e0a23f4ec395f02b13b
jdunnmon <jdunnmon@gmail.com>
2018-01-09 09:33:06 -0800

    using in-text regex to identify profiles

--- tutorials/fonduer/chtap/data_cleaning/cleaning_data_live_escort.py
+++ tutorials/fonduer/chtap/data_cleaning/cleaning_data_live_escort.py
@@ -1,24 +1,23 @@
-##10/10/2017
-## SAEIDEH
-### data cleaning for getting female scort profiles out of the data
+##01/09/2017
+## Jared Dunnmon
+### data cleaning for getting profiles out of live escort data
 
 
 import os 
 import re
 import shutil
-#data ="<!-- http://adultsearch.com/new-york/long-island/female-escorts/1096814 -->"
 
-#datapath="/Users/saeideh.shahrokh/Desktop/data-memex/snorkel/tutorials/fonduer/memex/data/myplugin_test_copy3/"
 datapath="/home/jdunnmon/research/re/projects/memex/data/s3/data_live_escort_20171206/"
-print(datapath)
 files = os.listdir(datapath)
 files = [f for f in files if f[-3:]!='crc']
 doc_name = []
+#using single rule based on existence of 'terReport' class
 for fn in files:
 	fname = fn[:-5]
 	with open(datapath+fn, mode ='r') as f:
 		for line in f.readlines():
-			if re.search(r'terReport',line):
+			#if re.search(r'terReport',line):
+                        if re.search(r'^read reviews$|^write a review$|^no ter review found$|^ter id$',line.lower()):
 				    doc_name.append(fn)
 								
 
